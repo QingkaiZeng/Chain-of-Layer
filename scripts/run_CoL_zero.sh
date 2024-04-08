98d53b96d6abad0263e0ca6f3464e7c5f0abca20
leopoldwhite <leopoldwhite@163.com>
2024-04-08 09:02:44 +0800

    Fix model name error in scripts

--- scripts/run_CoL_zero.sh
+++ scripts/run_CoL_zero.sh
@@ -3,7 +3,7 @@
 python infer.py \
   --openai_key your_openai_key \
   --taxo_name semeval_sci \
-  --model gpt-4-turbo-preview \
+  --model gpt-4-1106-preview \
   --numofExamples 5 \
   --run True \
   --save_path_model_response ./results/taxo_ChainofLayers_filter_zero/ \
